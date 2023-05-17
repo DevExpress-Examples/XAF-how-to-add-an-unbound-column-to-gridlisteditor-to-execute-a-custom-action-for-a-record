7d2d82c963599fa20d24e54a8734ee73ca5ab7b7
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-17 14:39:36 +0400

    xpo

---
+++ CS/XPO/ButtonInList/ButtonInList.Module/DatabaseUpdate/Updater.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.BaseImpl;
+using WinSolution.Module;
+
+namespace ButtonInList.Module.DatabaseUpdate;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Updating.ModuleUpdater
+public class Updater : ModuleUpdater {
+    public Updater(IObjectSpace objectSpace, Version currentDBVersion) :
+        base(objectSpace, currentDBVersion) {
+    }
+    public override void UpdateDatabaseAfterUpdateSchema() {
+        base.UpdateDatabaseAfterUpdateSchema();
+        var cnt = ObjectSpace.GetObjectsCount(typeof(Order), null);
+        if(cnt > 0 ) {
+            return;
+        }
+        for (int i = 0; i < 5; i++) {
+            Order o = ObjectSpace.CreateObject<Order>();
+            o.Name = string.Format("Order{0}", i);
+            if (i % 2 == 0) {
+                o.SimpleBusinessAction();
+            }
+        }
+
+        ObjectSpace.CommitChanges(); //Uncomment this line to persist created object(s).
+    }
+    public override void UpdateDatabaseBeforeUpdateSchema() {
+        base.UpdateDatabaseBeforeUpdateSchema();
+        //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
+        //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
+        //}
+    }
+}
---
+++ CS/XPO/ButtonInList/ButtonInList.Win/WinApplication.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Win.Utils;
+using DevExpress.ExpressApp.Xpo;
+
+namespace ButtonInList.Win;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Win.WinApplication._members
+public class ButtonInListWindowsFormsApplication : WinApplication {
+    public ButtonInListWindowsFormsApplication() {
+		SplashScreen = new DXSplashScreen(typeof(XafSplashScreen), new DefaultOverlayFormOptions());
+        ApplicationName = "ButtonInList";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        UseOldTemplates = false;
+        DatabaseVersionMismatch += ButtonInListWindowsFormsApplication_DatabaseVersionMismatch;
+        CustomizeLanguagesList += ButtonInListWindowsFormsApplication_CustomizeLanguagesList;
+    }
+    private void ButtonInListWindowsFormsApplication_CustomizeLanguagesList(object sender, CustomizeLanguagesListEventArgs e) {
+        string userLanguageName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+        if(userLanguageName != "en-US" && e.Languages.IndexOf(userLanguageName) == -1) {
+            e.Languages.Add(userLanguageName);
+        }
+    }
+    private void ButtonInListWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+			string message = "The application cannot connect to the specified database, " +
+				"because the database doesn't exist, its version is older " +
+				"than that of the application or its schema does not match " +
+				"the ORM data model structure. To avoid this error, use one " +
+				"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+			if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+				message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+			}
+			throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
